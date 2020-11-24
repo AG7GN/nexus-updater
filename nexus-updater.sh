be93feeb91a16bd0a6ed52ee4ee42a7d2aa409a5
Steve Magnuson <ag7gn.steve@gmail.com>
2020-11-24 13:19:05 -0800

    Update nexus-updater.sh

--- nexus-updater.sh
+++ nexus-updater.sh
@@ -417,7 +417,15 @@ function CheckDepInstalled() {
 	# installed.
 	# Takes 1 argument: a string containing the apps to check with apps separated by space
 	#MISSING=$(dpkg --get-selections $1 2>&1 | grep -v 'install$' | awk '{ print $6 }')
-	MISSING=$(dpkg-query -W -f='${Package} ${Status}\n' $1 2>&1 | grep 'not-installed$' | awk '{ print $1 }')
+	#MISSING=$(dpkg-query -W -f='${Package} ${Status}\n' $1 2>&1 | grep 'not-installed$' | awk '{ print $1 }')
+	MISSING=""
+   for P in $1
+   do
+      if apt-cache policy $P | grep -q "Installed: (none)"
+      then
+         MISSING+="$P "
+      fi
+   done
 	if [[ ! -z $MISSING ]]
 	then
 		sudo apt-get -y install $MISSING || AptError "$MISSING"
