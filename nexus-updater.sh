7f42f713effdb8e5be4cd4b8254f4508420a9133
Steve Magnuson <ag7gn.steve@gmail.com>
2020-12-05 15:32:55 -0800

    Update nexus-updater.sh

--- nexus-updater.sh
+++ nexus-updater.sh
@@ -42,7 +42,7 @@
 #%    
 #================================================================
 #- IMPLEMENTATION
-#-    version         ${SCRIPT_NAME} 2.0.10
+#-    version         ${SCRIPT_NAME} 2.0.11
 #-    author          Steve Magnuson, AG7GN
 #-    license         CC-BY-SA Creative Commons License
 #-    script_id       0
@@ -823,7 +823,7 @@ fi
 sudo sed -i 's/^#deb-src/deb-src/' /etc/apt/sources.list
 sudo sed -i 's/^#deb-src/deb-src/' /etc/apt/sources.list.d/raspi.list
 
-CheckDepInstalled "extra-xdg-menus bc dnsutils libgtk-3-bin jq moreutils exfat-utils build-essential autoconf automake libtool checkinstall git aptitude"
+CheckDepInstalled "extra-xdg-menus bc dnsutils libgtk-3-bin jq xdotool moreutils exfat-utils build-essential autoconf automake libtool checkinstall git aptitude"
 
 # Check age of apt cache. Run apt update if more than 2 hours old
 LAST_APT_UPDATE=$(stat -c %Z /var/lib/apt/lists/partial)
